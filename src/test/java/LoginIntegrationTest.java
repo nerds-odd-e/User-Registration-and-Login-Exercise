a099eb75ba6245957b69725bd9cc06d303debdb9
josephyao <dermott@126.com>
2014-02-11 15:29:55 +0800

    Add failed login integration test

--- src/test/java/LoginIntegrationTest.java
+++ src/test/java/LoginIntegrationTest.java
@@ -4,7 +4,6 @@ import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.servlet.ServletException;
@@ -21,26 +20,45 @@ public class LoginIntegrationTest {
 
     private DBCollection users;
 
+    LoginServlet servlet = new LoginServlet();
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+
     @Before
-    public void initialUsersCollection() throws Exception {
+    public void createUser() throws Exception {
         MongoClient client = new MongoClient("192.168.56.103");
         DB db = client.getDB("test");
         users = db.getCollection("Users");
+        BasicDBObject user = new BasicDBObject("username", "username").
+                append("password", "password");
+        users.insert(user);
     }
 
     @Test
     public void successful_login() throws ServletException, IOException {
-        createUser();
-        LoginServlet servlet = new LoginServlet();
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("username");
-        when(request.getParameter("password")).thenReturn("password");
+        setRequestUsernameAndPasswordParameters("username", "password");
 
         servlet.doPost(request, response);
 
         verify(response).sendRedirect("index.jsp?result=SuccessfulLogin");
+    }
+
+    @Test
+    public void failed_login() throws ServletException, IOException {
+        setRequestUsernameAndPasswordParameters("username", "wrongPassword");
+
+        servlet.doPost(request, response);
+
+        verify(response).sendRedirect("index.jsp?result=FailedLogin");
+    }
+
+    private void setRequestUsernameAndPasswordParameters(String username, String password) {
+        setRequestParameter("username", username);
+        setRequestParameter("password", password);
+    }
+
+    private void setRequestParameter(String key, String value) {
+        when(request.getParameter(key)).thenReturn(value);
     }
 
     @After
@@ -48,9 +66,4 @@ public class LoginIntegrationTest {
         users.drop();
     }
 
-    private void createUser() {
-        BasicDBObject user = new BasicDBObject("username", "username").
-                append("password", "password");
-        users.insert(user);
-    }
 }
